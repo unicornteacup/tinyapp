9ef4e6da2ef899e680a28f9971aa31e618d47437
Caroline <carolinejepps@gmail.com>
2020-07-07 21:51:26 +0000

    Added app.post to delete urls

--- express_server.js
+++ express_server.js
@@ -73,6 +73,13 @@ app.get("/u/:shortURL", (req, res) => {
   res.redirect(longURL);
 });
 
+app.post("/urls/:shortURL/delete", (req, res) => {
+  console.log(req.params)
+  let shortURL = req.params.shortURL;
+  delete urlDatabase[shortURL];
+  res.redirect("/urls");
+})
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
