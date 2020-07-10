6f3306142a7ca985b91afbb4725110d3c436a6e3
Caroline <carolinejepps@gmail.com>
2020-07-10 06:29:41 +0000

    Tidied code and linted

--- express_server.js
+++ express_server.js
@@ -1,201 +1,172 @@
+// REQUIREMENTS
+
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 const bodyParser = require("body-parser");
-//const cookieParser = require('cookie-parser');
 const bcrypt = require('bcrypt');
 const helper = require("./helpers");
 const cookieSession = require('cookie-session');
+
+// APP.USE REQUIREMENTS
 
 app.use(cookieSession({
   name: 'session',
   keys: ['thisismysuperlongstringtouseforcookiesessions', 'thisisasecondlongstring']
 }));
+
 app.use(bodyParser.urlencoded({extended: true}));
-//app.use(cookieParser());
+
 app.set("view engine", "ejs");
+
+// TEST DATABASES
 
 const urlDatabase = {
   "b2xVn2": { longURL: "http://www.lighthouselabs.ca", userID: "userRandomID"},
   "9sm5xK": { longURL: "http://www.google.com", userID: "user2RandomID"}
 };
 
-const users = { 
+const users = {
   "userRandomID": {
-    id: "userRandomID", 
-    email: "user@example.com", 
+    id: "userRandomID",
+    email: "user@example.com",
     password: "purple-monkey-dinosaur"
   },
- "user2RandomID": {
-    id: "user2RandomID", 
-    email: "user2@example.com", 
+  "user2RandomID": {
+    id: "user2RandomID",
+    email: "user2@example.com",
     password: "dishwasher-funk"
   }
 };
 
-// GET FUNCTIONS
+// LOGIN, REGISTER & LOGOUT ENDPOINTS
 
-app.get("/", (req, res) => {
-  res.send("Hello!");
-});
-
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
-});
-
-// app.get("/hello", (req, res) => {
-//   res.send("<html><body>Hello <b>World</b></body></html>\n");
-// });
 app.get("/register", (req, res) => {
-  let templateVars = { 
+  let templateVars = {
     email: ""
   };
   res.render("register", templateVars);
 });
 
 app.get("/login", (req, res) => {
-  let templateVars = { 
+  let templateVars = {
     email: ""
   };
   res.render("login", templateVars);
 });
 
 app.post("/login", (req, res) => {
-
-  console.log(req.body.email);
   const email = req.body.email;
   const password = req.body.password;
-  console.log(password)
-  
   if (!req.body.email) {
     return res.send("Error 400, No email entered");
-  } 
+  }
   if (!req.body.password) {
     return res.send("Error 400, No password entered");
-  } 
-  const user = helper.getUserByEmail(email, users)
-  console.log(user);
+  }
+  const user = helper.getUserByEmail(email, users);
   if (user) {
     if (bcrypt.compareSync(password, user.hashedPassword)) {
       req.session.user_id = user.id;
       res.redirect("/urls");
     } else {
       res.send("Error Password incorrect");
-    } 
-  } else{
+    }
+  } else {
     res.send("Error 403, User not found");
   }
-}) 
+});
 
 app.post("/register", (req, res) => {
-  
   if (!req.body.name) {
     res.send("Error 400, No email entered");
   } else if (!req.body.password) {
     res.send("Error 400, No password entered");
   } else if (helper.getUserByEmail(req.body.name, users)) {
     res.send("Error 400, user already exists");
-  } else {  
+  } else {
     let email = req.body.name;
     let password = req.body.password;
     const hashedPassword = bcrypt.hashSync(password, 10);
-    const id = helper.generateRandomString();;
-    users[id] = {id: id, email: email, hashedPassword: hashedPassword}
-    console.log(users)
-    // add cookie for user_id
+    const id = helper.generateRandomString();
+    users[id] = {id: id, email: email, hashedPassword: hashedPassword};
     req.session.user_id = id;
-    //redirect
     res.redirect("/urls");
   }
-})
+});
 
 app.post("/logout", (req, res) => {
-  //console.log(req.body.username);
-  //const username = req.body.username;
   req.session = null;
-  
   res.redirect("/login");
-})
-
-app.get("/urls", (req, res) => {
-  if (!req.session.user_id) {
-    res.redirect("/notloggedin");
-  } else {
-    console.log(req.session.user_id);
-    urlList = helper.urlsForUser(req.session.user_id, urlDatabase)
-
-    let templateVars = { 
-      urls: urlList,
-      email: users[req.session.user_id].email
-    };
-  res.render("urls_index", templateVars);
-  };
 });
 
 app.get("/notloggedin", (req, res) => {
-  let templateVars = { 
+  let templateVars = {
     email: ""
   };
   res.render("logReg", templateVars);
 });
 
+//  CREATING AND VIEWING URLS
+
 app.get("/urls/new", (req, res) => {
   if (!req.session.user_id) {
     res.redirect("/login");
   } else {
-    let templateVars = { 
+    let templateVars = {
       urls: urlDatabase,
       email: users[req.session.user_id].email
     };
     res.render("urls_new", templateVars);
-  };
+  }
 });
 
+app.get("/urls", (req, res) => {
+  if (!req.session.user_id) {
+    res.redirect("/notloggedin");
+  } else {
+    const urlList = helper.urlsForUser(req.session.user_id, urlDatabase);
+    let templateVars = {
+      urls: urlList,
+      email: users[req.session.user_id].email
+    };
+    res.render("urls_index", templateVars);
+  }
+});
 
-// How do I save it properly to the database and do the redirect?
 app.post("/urls", (req, res) => {
-  console.log(req.body); // Log the POST request body to the console
-           // Respond with 'Ok' (we will replace this)
-  let longURL = req.body.longURL
-  let userID = users[req.session.user_id].id
-  console.log(longURL)
-  console.log(userID)
+  let longURL = req.body.longURL;
+  let userID = users[req.session.user_id].id;
   const shortURL = helper.generateRandomString();
-  urlDatabase[shortURL] = {longURL: longURL, userID: userID}
-  console.log(urlDatabase);
+  urlDatabase[shortURL] = {longURL: longURL, userID: userID};
   res.redirect(`/urls/${shortURL}`);
-  
 });
 
 app.get("/urls/:shortURL", (req, res) => {
   let shortURL = req.params.shortURL;
-  const urlList = helper.urlsForUser(users[req.session.user_id], urlDatabase)
-  console.log(urlList)
+  const urlList = helper.urlsForUser(users[req.session.user_id], urlDatabase);
   if (!req.session.user_id) {
     res.redirect("/notloggedin");
   } else if (urlList[shortURL].userID !== users[req.session.user_id]) {
-    res.send(`This URL does not belong to ${users[req.session.user_id].email}`)
+    res.send(`This URL does not belong to ${users[req.session.user_id].email}`);
   } else {
-    
-    console.log(shortURL)
-    console.log(urlDatabase[shortURL])
     let longURL = urlDatabase[shortURL]["longURL"];
-    
-    let templateVars = { 
-      shortURL: shortURL, 
+    let templateVars = {
+      shortURL: shortURL,
       longURL: longURL,
       email: users[req.session.user_id].email
     };
-  res.render("urls_show", templateVars);
-  };
+    res.render("urls_show", templateVars);
+  }
 });
 
 app.get("/u/:shortURL", (req, res) => {
   let shortURL = req.params.shortURL;
-  console.log(urlDatabase[shortURL]["longURL"])
-  const longURL = urlDatabase[shortURL]["longURL"];// const longURL = ...
+  const longURL = urlDatabase[shortURL]["longURL"];
   res.redirect(longURL);
 });
+
+// EDIT OR DELETE URL
 
 app.post("/urls/:shortURL/delete", (req, res) => {
   const urlList = helper.urlsForUser(req.session.user_id, urlDatabase);
@@ -203,12 +174,11 @@ app.post("/urls/:shortURL/delete", (req, res) => {
   if (!req.session.user_id || urlDatabase[shortURL].userID !== req.session.user_id) {
     res.send("This URL can only be deleted by the owner");
   } else {
-    console.log(req.params)
     let shortURL = req.params.shortURL;
     delete urlDatabase[shortURL];
     res.redirect("/urls");
   }
-})
+});
 
 app.post("/urls/:shortURL/update", (req, res) => {
   const urlList = helper.urlsForUser(req.session.user_id, urlDatabase);
@@ -216,16 +186,11 @@ app.post("/urls/:shortURL/update", (req, res) => {
   if (!req.session.user_id || urlList[shortURL].userID !== req.session.user_id) {
     res.send("This URL can only be edited by the owner");
   } else {
-    console.log(req.body)
-
-    console.log(shortURL.newURL)
     let longURL = req.body.updateUrl;
     urlDatabase[shortURL] = longURL;
     res.redirect("/urls");
   }
-  
-  
-})
+});
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
